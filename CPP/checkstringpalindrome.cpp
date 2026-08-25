d344faecfd4cd84e18b4045ea3ad29e7d33f3e8d
shahsurajdukhan <surajsah1903@gmail.com>
2026-08-25 22:33:46 +0530

    solved the question of string check string is palindrome or not

---
+++ CPP/checkstringpalindrome.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPalindrome(string str) {
+    int left = 0;
+    int right = str.length() - 1;
+    
+    while (left < right) {
+        if (str[left] != str[right]) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+int main() {
+    string str = "madam";
+
+    if (isPalindrome(str)) {
+        cout << "Palindrome" << endl;
+    }
+    else {
+        cout << "Not a Palindrome" << endl;
+    }
+
+    return 0;
+}
