bdf3b8e072799774bdcaba943fb55ede53a55596
shahsurajdukhan <surajsah1903@gmail.com>
2026-09-04 17:58:33 +0530

    returned string type in method of java function

--- JAVA/javareturn.java
+++ JAVA/javareturn.java
@@ -1,9 +1,22 @@
+// public class javareturn {
+//     static int myMethod(int x) {
+//         return 5 + x;
+//     }
+
+//     public static void main(String[] args) {
+//         System.out.println(myMethod(15));
+//     }
+// }
+
+// Let's try to do it with the string type of return value in methods.
+
 public class javareturn {
-    static int myMethod(int x) {
-        return 5 + x;
+    static String Naam(String kam) {
+        return "Hi Buddy " + kam; 
     }
 
-    public static void main(String[] args) {
-        System.out.println(myMethod(15));
+    public static void main(String [] args) {
+        
+        System.out.println(Naam("Suraj Sah"));
     }
 }
