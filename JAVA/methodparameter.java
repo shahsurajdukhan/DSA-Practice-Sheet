9c20ff58c1be7aeda8baaefc14a265d185b1eb33
shahsurajdukhan <surajsah1903@gmail.com>
2026-09-04 17:28:59 +0530

    Learned about method parameter today

---
+++ JAVA/methodparameter.java
@@ -0,0 +1,14 @@
+public class methodparameter {
+    static void myMethod(String firstname) {
+        System.out.println(firstname + "Sah");
+
+    }
+
+
+public static void main(String[] args) {
+    myMethod("Suraj ");
+    myMethod("Anjana ");
+    myMethod("Balaji ");
+}
+
+}
