9132727d1380c061afd2dd0714d6a89cc31e6d27
shahsurajdukhan <surajsah1903@gmail.com>
2026-09-04 17:20:24 +0530

    Created a method in Java

---
+++ JAVA/practice.java
@@ -0,0 +1,14 @@
+public class practice {
+
+// let's create methods in java
+    public static void MyAudio() {
+        System.out.println("Audio iss Playing Here Perfectly");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Javaa System");
+        MyAudio();
+        System.out.println("Check Check Working the Audio System?");
+        MyAudio();
+    } 
+}
