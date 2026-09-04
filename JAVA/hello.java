71f8f46945f4491b2aed66c694c112175f765651
shahsurajdukhan <surajsah1903@gmail.com>
2026-09-04 11:26:29 +0530

    It's the java learning self paced folder

---
+++ JAVA/hello.java
@@ -0,0 +1,13 @@
+class hello 
+{
+    public static void main(String a[])
+    {
+       byte b = 3;
+       int i = 120;
+
+       b = (byte) i;
+
+       System.out.println("Hello bhai we got the direct access to compile and run the java file");
+    }
+
+}
