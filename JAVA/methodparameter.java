afc7ac92d0871e2836f346761e98ef0ec74e3d6b
shahsurajdukhan <surajsah1903@gmail.com>
2026-09-04 17:38:50 +0530

    learned about the multiple parameters in a method

--- JAVA/methodparameter.java
+++ JAVA/methodparameter.java
@@ -1,14 +1,34 @@
+// public class methodparameter {
+//     static void myMethod(String firstname) {
+//         System.out.println(firstname + "Sah");
+
+//     }
+
+
+// public static void main(String[] args) {
+//     myMethod("Suraj ");
+//     myMethod("Anjana ");
+//     myMethod("Balaji ");
+// }
+
+// }
+
+
+
+// Now we are going to learn about multiple parameters in the same method it's the same with the separated comma values.
+
+import java.util.jar.Attributes.Name;
+
 public class methodparameter {
-    static void myMethod(String firstname) {
-        System.out.println(firstname + "Sah");
+    static void NameParameter(String firstname, int age) {
+        System.out.println("Hi " + firstname + "! You are " + age + " Years old! :)");
 
     }
 
+    public static void main(String[] args) {
+        NameParameter("Suraj", 12);
+        NameParameter("Dukhan", 54);
+        NameParameter("Bhagmati", 47);
 
-public static void main(String[] args) {
-    myMethod("Suraj ");
-    myMethod("Anjana ");
-    myMethod("Balaji ");
-}
-
+    }
 }
