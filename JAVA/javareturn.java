35c6182577ae532c3787f81f7c2a69fedc1bb2f9
shahsurajdukhan <surajsah1903@gmail.com>
2026-09-04 17:51:13 +0530

    methods can have a return type also

---
+++ JAVA/javareturn.java
@@ -0,0 +1,9 @@
+public class javareturn {
+    static int myMethod(int x) {
+        return 5 + x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myMethod(15));
+    }
+}
