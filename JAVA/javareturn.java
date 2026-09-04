3b5f9ed50d86943014707606a1585bc05ef179d9
shahsurajdukhan <surajsah1903@gmail.com>
2026-09-04 18:06:29 +0530

    Solved the game of for loop in main and int return method

--- JAVA/javareturn.java
+++ JAVA/javareturn.java
@@ -10,13 +10,29 @@
 
 // Let's try to do it with the string type of return value in methods.
 
+// public class javareturn {
+//     static String Naam(String kam) {
+//         return "Hi Buddy " + kam; 
+//     }
+
+//     public static void main(String [] args) {
+        
+//         System.out.println(Naam("Suraj Sah"));
+//     }
+// }
+
+
+// Let's solve the double game - here is the question
+// using a method that returns a value, to show the double of the numbers 1 through 5 (using a for loop):
+
 public class javareturn {
-    static String Naam(String kam) {
-        return "Hi Buddy " + kam; 
+    static int doublegame(int num) {
+        return num * 2;
     }
 
-    public static void main(String [] args) {
-        
-        System.out.println(Naam("Suraj Sah"));
+    static void main(String[] args) {
+        for (int i =1; i<11; i++) {
+            System.out.println("Double of " + i + " is " + doublegame(i));
+        }
     }
 }
