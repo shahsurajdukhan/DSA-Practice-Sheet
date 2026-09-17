b425132e395f9d470b439c0d19414404dfa7272d
Shah Suraj Dukhan <i.suraj.in@gmail.com>
2026-09-17 13:45:35 +0530

    Update hello.java

--- JAVA/hello.java
+++ JAVA/hello.java
@@ -1,13 +1,7 @@
 class hello 
 {
-    public static void main(String a[])
-    {
-       byte b = 3;
-       int i = 120;
-
-       b = (byte) i;
-
-       System.out.println("Hello bhai we got the direct access to compile and run the java file");
-    }
-
+  public static void main (String a[]) {
+  
+  system.out.println("Hello bhai");
+}
 }
